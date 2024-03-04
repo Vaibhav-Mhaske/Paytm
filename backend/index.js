9621196a1ffc75e14ce10c23397fe10c2f89e43c
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-03-04 23:08:17 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
+// Importing the required modules
 const express = require("express");
 const cors = require("cors");
 
+// Creating an instance of the Express application
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+// Middleware setup
+app.use(express.json()); // Parse incoming JSON data
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 
+// Importing the main router module
 const mainRouter = require("./routes/index");
 
-app.use("/api/v1", mainRouter)
+// Mounting the main router at the "/api/v1" endpoint
+app.use("/api/v1", mainRouter);
+
+// Starting the server and listening on port 3000
 app.listen(3000, () => {
-    console.log("Server is running on port 3000 🚀")
-})
+    console.log("Server is running on port 3000 🚀");
+});
