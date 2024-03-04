aae2954374469cd0c0ed1d01fae882e84331e4d1
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-03-04 22:39:55 +0530

    mongoose schema

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
+// Importing the mongoose library
 const mongoose = require('mongoose');
 
+// Connecting to the MongoDB database using the provided connection string
 mongoose.connect("mongodb+srv://vaibhav:8791435484@cluster0.qenvmnm.mongodb.net/paytm")
 
+// Defining the schema for the user collection in the database
 const mongooseSchema = mongoose.Schema({
     username: String,
     password: String,
@@ -9,9 +12,10 @@ const mongooseSchema = mongoose.Schema({
     lastName: String
 })
 
-// create model for the schema
+// Creating a model for the user schema
 const User = mongoose.model('User', mongooseSchema);
 
+// Exporting the User model to be used in other files
 module.exports = {
     User
 }
